88b4e1e8d7a0a49b5efa65b5739fb7cf61bf08ea
朱梅 <1540588534@qq.com>
2019-12-13 21:55:55 +0800

    Study how to use the route

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.shortcuts import render
 
 # Create your models here.
+
+# from wagtail.contrib.routable_page.models import RoutablePageMixin, route
 from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
 from wagtail.core.models import Page
 from wagtail.core.fields import StreamField
@@ -9,12 +12,12 @@ from wagtail.images.edit_handlers import ImageChooserPanel
 from streams import blocks
 
 
-class BlogListPage(Page):
+class BlogListPage(RoutablePageMixin, Page):
     template = "blog/blog_list_page.html"
     custom_title = models.CharField(
         max_length=100,
-        blank=False,
-        null=False,
+        blank=False,  # 针对表单，若为true，表示你的表单填写该字段的时候可以不填
+        null=False,  # 针对数据库而言，为True，表示数据库的该字段可以以为空
         help_text='Overwrites the default title',
     )
     content_panels = Page.content_panels + [
@@ -24,11 +27,19 @@ class BlogListPage(Page):
     def get_context(self, request, *args, **kwargs):
         """Adding custom stuff to our context"""
         context = super().get_context(request, *args, **kwargs)
-        # context['extra'] = "Read all about it"
+        context['posts] = BlogDetailPage.object.live().public()
         return context
+
+    @route(r'^latest/$')
+    def latest_blog_posts(self, request, *args, ***kwargs):
+        context = self.get_context(request, & args, **kwargs)
+
+        return render(request, "blog/latest_posts.html", context)
 
 
 class BlogDetailPage(Page):
+
+    template = "blog/blog_detail_page.html"
     custom_title = models.CharField(
         max_length=100,
         blank=False,
@@ -41,7 +52,6 @@ class BlogDetailPage(Page):
         null=True,
         related_name="+",
         on_delete=models.SET_NULL,
-
     )
     content = StreamField(
         [

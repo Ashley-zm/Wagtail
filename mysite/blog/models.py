31132e0869c3ae444d8e5b50404e0d70ed6c6038
朱梅 <1540588534@qq.com>
2019-12-31 13:19:00 +0800

    add RoutablePageMixin

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,14 +2,17 @@ from django.db import models
 from django.shortcuts import render
 
 # Create your models here.
-
-# from wagtail.contrib.routable_page.models import RoutablePageMixin, route
-from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
 from wagtail.core.models import Page
+from wagtail.contrib.routable_page.models import RoutablePageMixin, route
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
 from wagtail.core.fields import StreamField
 from wagtail.images.edit_handlers import ImageChooserPanel
 
 from streams import blocks
+
+# RoutablePageMixin混合为页面提供了一种方便的方式来响应具有不同视图的多个子URL
+# 默认情况下，存在r'^ $'的路由，该路由完全像常规Page那样提供内容。
+# 可以通过在继承类的任何其他方法上使用@route（r'^ $'）来覆盖它。
 
 
 class BlogListPage(RoutablePageMixin, Page):
@@ -27,13 +30,17 @@ class BlogListPage(RoutablePageMixin, Page):
     def get_context(self, request, *args, **kwargs):
         """Adding custom stuff to our context"""
         context = super().get_context(request, *args, **kwargs)
-        context['posts] = BlogDetailPage.object.live().public()
+        context["posts"] = BlogDetailPage.objects.live().public()
+        # context["regular_context_var"] = "I like the page"
+        context["a_special_link"] = self.reverse_subpage('latest_posts')
         return context
-
-    @route(r'^latest/$')
-    def latest_blog_posts(self, request, *args, ***kwargs):
-        context = self.get_context(request, & args, **kwargs)
-
+//添加路径
+    @route(r'^latest/$', name="latest_posts")
+    def latest_blog_posts_only_shows_last_5(self, request, *args, **kwargs):
+        context = self.get_context(request, *args, **kwargs)
+        context["latest_posts"] = BlogDetailPage.objects.live().public()
+        context["name"] = "Kalaob Tauline"
+        context["website"] = "Learn Kalaob Tauline"
         return render(request, "blog/latest_posts.html", context)
 
 

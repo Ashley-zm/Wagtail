f288b11ef3da5f7f2f6483be1a6a2e50a74ea70a
朱梅 <1540588534@qq.com>
2020-04-01 22:30:12 +0800

    cooment csrf

--- mysite/mysite/settings/base.py
+++ mysite/mysite/settings/base.py
@@ -65,7 +65,7 @@ MIDDLEWARE = [
 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -73,7 +73,7 @@ MIDDLEWARE = [
 
     'wagtail.core.middleware.SiteMiddleware',
     'wagtail.contrib.redirects.middleware.RedirectMiddleware',
-    
+
 ]
 
 ROOT_URLCONF = 'mysite.urls'
